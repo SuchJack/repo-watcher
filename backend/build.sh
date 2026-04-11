b9c842c39ff4eb0dafdedb40c16f7f5a4a8b3d53
SuchJack <2640808535@qq.com>
2026-04-11 10:40:27 +0800

    build(docker): 配置项目 Docker 镜像构建和部署流程
    - 添加 .local-config 到 .gitignore 以忽略本地配置文件
    - 为后端服务创建 build.sh 脚本用于 Docker 镜像构建
    - 为前端服务创建 build.sh 脚本用于 Docker 镜像构建
    - 修改 docker-compose.yml 使用预构建的镜像替代本地构建
    - 为后端服务添加 push.sh 脚本用于推送镜像到阿里云容器 registry
    - 为前端服务添加 push.sh 脚本用于推送镜像到阿里云容器 registry
    - 配置阿里云容器 registry 的登录认证和镜像推送流程

---
+++ backend/build.sh
@@ -0,0 +1,5 @@
+# 普通镜像构建，随系统版本构建 amd/arm
+docker build -t suchjack/repo-monitor-backend:1.0 -f ./Dockerfile .
+
+# 兼容 amd、arm 构建镜像
+# docker buildx build --load --platform liunx/amd64,linux/arm64 -t suchjack/repo-monitor-frontend:1.0 -f ./Dockerfile . --push
